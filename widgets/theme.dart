0d9210b1c9eccddeb21f6cad28cf8af840d873b6
Dharshan1911 <157696269+Dharshan1911@users.noreply.github.com>
2024-09-08 23:32:45 +0530

    Create theme.dart

---
+++ widgets/theme.dart
@@ -0,0 +1,9 @@
+// theme.dart
+
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primaryColor = Color(0xFF2196F3);
+  static const Color accentColor = Color(0xFF8BC34A);
+  static const Color warningColor = Color(0xFFF57C00);
+}
